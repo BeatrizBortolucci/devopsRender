cc336d916e7fa82c25da1cb9ab79232c71943d0b
Beatriz <beatriz.domingos@germinare.org.br>
2023-06-06 16:14:15 -0300

    Primeiro Commit

---
+++ atv1.js
@@ -0,0 +1,18 @@
+var tempMediaAtual = Number(prompt(`Qual é a temperatura média atual? `));
+
+var quantAnos = Number(prompt(`Qual é a quantidade de anos? `));
+
+var taxaTemp = Number(prompt(`Qual é a taxa de crescimento/diminuição anual da temperatura? `));
+
+var cont = 0;
+
+function Contador() {
+ (tempMediaAtual += tempMediaAtual / 100 * taxaTemp);
+}
+
+while(cont < quantAnos) {
+    Contador() 
+    cont++ 
+}
+
+alert(`A temperatura média do planeta é de ${tempMediaAtual}`);
---
+++ atv2.js
@@ -0,0 +1,21 @@
+var valorImovel = Number(prompt(`Qual é o valor do imóvel?`));
+
+var investimentoMensal = Number(
+  prompt(`Qual é o valor do investimento mensal?`)
+);
+
+var TaxaMensal = Number(prompt(`Qual é o valor da taxa mensal?`));
+
+var valorPago = 0;
+var tempo = -1;
+var valorMinimo = valorImovel / 50;
+
+while(valorPago < valorImovel) {
+    valorPago = investimentoMensal + (valorPago + (valorPago / 100) * TaxaMensal);
+    tempo++
+}
+if(investimentoMensal < valorMinimo) {
+    alert(`O investimento não é viável`)
+}else {
+    alert(`O tempo necessário será de ${tempo} meses.`);
+}
